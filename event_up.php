58235c07234bc9adffee77c82a1cdce87a0a9f1d
lizuoyue <li.zuoyue@foxmail.com>
2017-04-12 17:33:58 +0200

    a

--- event_up.php
+++ event_up.php
@@ -1,26 +1,39 @@
 <?php
-	require_once('../../../wp-load.php');
-	if(isset($_GET['pid'])){
-    	$pid = $_GET['pid'];
-    	$custom_fields = get_post_custom($pid);
-    	?>
-			<header class="event-header"><b> Upcoming Events </b></header>
-            <div id="event-detail">
-                <img src = "<?php  echo wp_get_attachment_url($custom_fields["event_image"][0]) ?>" alt="" />
-                <?php $sd = new DateTime($custom_fields["event_stime"][0]);
-                $ed = new DateTime($custom_fields["event_etime"][0]); ?>
-            <div id="event-detail-text">
-                <div>
-                    <h2><?php  echo $custom_fields["event_pname"][0] ?></h2>
-                </div>
-                <div>
-                    <p><?php  echo $sd->format("Y-m-d h:i") ?> -  <?php  echo $ed->format("Y-m-d h:i") ?></p>
-                </div>
-                <div>
-                    <?php  echo $custom_fields["event_description"][0] ?>
-                </div>
-            </div>
-            <div class="x-clear"> </div>
-    	
-    <?php } ?>
-	
+
+require_once('../../../wp-load.php');
+if(isset($_GET['pid'])){
+	$pid = $_GET['pid'];
+	$custom_fields = get_post_custom($pid);
+	?>
+	<header class="event-header"><b> Upcoming Events </b></header>
+	<div id="event-detail">
+		<div id="event-detail-img">
+			<img src = "<?php echo wp_get_attachment_url($custom_fields["event_image"][0]) ?>" alt="" />
+			<?php $sd = new DateTime($custom_fields["event_stime"][0]);
+			$ed = new DateTime($custom_fields["event_etime"][0]); ?>
+		</div>
+		<div id="event-detail-text">
+			<div>
+				<h2><?php echo $custom_fields["event_pname"][0] ?></h2>
+			</div>
+			<div>
+				<?php
+				if ($custom_fields["event_etime"][0]=="")
+				{
+				?>
+				<p><?php echo $sd->format("Y-m-d H:i") ?></p>	
+				<?php
+				} else {?>
+				<p><?php echo $sd->format("Y-m-d H:i") ?> - <?php echo $ed->format("Y-m-d H:i") ?></p>
+				<?php
+				}
+				?>
+			</div>
+
+			<div>
+				<?php echo $custom_fields["event_description"][0] ?>
+			</div>
+		</div>
+	</div>
+	<div class="x-clear"></div>
+<?php } ?>
--- index.php
+++ index.php
@@ -314,8 +314,17 @@
 							<figcaption>
 								<?php $sd = new DateTime($eventlist_upcoming[$i]["event_stime"][0]); ?>
 								<?php $ed = new DateTime($eventlist_upcoming[$i]["event_etime"][0]); ?>
-								<h3><a href="" onclick="readMoreUp(<?php echo $eventlist_upcoming[$i]["pid"][0] ?>); return false;"><?php  echo $eventlist_upcoming[$i]["event_pname"][0] ?> </a></h3>
-								<h2><?php  echo $sd->format("Y-m-d h:i") ?> -  <?php  echo $ed->format("Y-m-d h:i") ?> </h2>
+								<h3><a href="" onclick="readMoreUp(<?php echo $eventlist_upcoming[$i]["pid"][0] ?>); return false;"><?php echo $eventlist_upcoming[$i]["event_pname"][0] ?></a></h3>
+								<?php if ($eventlist_upcoming[$i]["event_etime"][0]=="")
+								{?>
+								<a href=""><h2><?php echo $sd->format("Y-m-d H:i") ?></h2></a>	
+								<?php
+								}
+								else
+								{
+								?> 
+								<a href=""><h2><?php echo $sd->format("Y-m-d H:i") ?></br><?php echo $ed->format("Y-m-d H:i") ?></h2></a>
+								<?php }?>
 							</figcaption>
 							<article class="event-content">
 								<p><?php print get_abstract($eventlist_upcoming[$i]["event_description"][0]);?> <a href="" onclick="readMoreUp(<?php echo $eventlist_upcoming[$i]["pid"][0] ?>); return false;">[Read More]</a></p>
@@ -335,16 +344,15 @@
 								<?php $ed = new DateTime($eventlist_past[$i]["event_etime"][0]); ?>
 								<h3><a href="" onclick="readMorePast(<?php echo $eventlist_past[$i]["pid"][0] ?>); return false;"><?php  echo $eventlist_past[$i]["event_pname"][0] ?></a>
 								</h3>
-								<?php  if ($eventlist_past[$i]["event_etime"][0]=="")
+								<?php if ($eventlist_past[$i]["event_etime"][0]=="")
 								{?>
-								<a href=""><h2><?php  echo $sd->format("Y-m-d h:i") ?>  </h2></a>	
+								<a href=""><h2><?php  echo $sd->format("Y-m-d H:i") ?></h2></a>	
 								<?php
 								}
 								else
 								{
 								?> 
-
-								<a href=""><h2><?php  echo $sd->format("Y-m-d h:i") ?> -  <?php  echo $ed->format("Y-m-d h:i") ?> </h2></a>
+								<a href=""><h2><?php  echo $sd->format("Y-m-d H:i") ?></br><?php  echo $ed->format("Y-m-d H:i") ?> </h2></a>
 								<?php }?>
 							</a>
 						</figure>
